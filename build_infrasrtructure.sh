23d28c713e17b31b60b57016d915b6ffffcf9c39
Miniplop <kevinj@theodo.fr>
2018-12-09 13:52:35 +0100

    reformat script

--- build_infrasrtructure.sh
+++ build_infrasrtructure.sh
@@ -53,6 +53,13 @@ then
     exit
 fi
 
-aws cloudformation deploy --profile ${PROFILE} --stack-name ${PROJECT_NAME}-ecr-repository --template-file infrastructure/ecr-repository.yml --region ${REGION} --no-fail-on-empty-changeset --parameter-overrides RepositoryName=${PROJECT_NAME}-ecr-repository
+# Create a ECS Repository for dockerfile
+aws cloudformation deploy \
+    --profile ${PROFILE} \
+    --stack-name ${PROJECT_NAME}-ecr-repository \
+    --template-file infrastructure/ecs-repository.yml \
+    --region ${REGION} \
+    --no-fail-on-empty-changeset \
+    --parameter-overrides RepositoryName=${PROJECT_NAME}-ecr-repository
 
-aws cloudformation describe-stacks --stack-name ${PROJECT_NAME}-ecr-repository --region ${REGION} --profile ${PROFILE} --output text --query 'Stacks[].Outputs[]' | tr '\t' '='
+export $(aws cloudformation describe-stacks --stack-name ${PROJECT_NAME}-ecr-repository --region ${REGION} --profile ${PROFILE} --output text --query 'Stacks[].Outputs[]' | tr '\t' '=')
